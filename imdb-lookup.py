d83a4ea514ff959eb3a379e9ae3f364e79e66623
shanksweb <shanksmail@gmail.com>
2016-01-30 13:15:52 +0530

    update to imdb-lookup

--- imdb-lookup.py
+++ imdb-lookup.py
@@ -1,18 +1,6 @@
-#-------------------------------------------------------------------------------
-# Name:        imdb-lookup
-# Purpose:
-#
-# Author:      manoj mj
-#
-# Created:
-# Copyright:   (c) www.manojmj.com
-# Licence:
-#-------------------------------------------------------------------------------
-
-import urllib
+import urllib.request, urllib.parse, urllib.error
 import json
 import webbrowser
-
 import sys
 
 def finder(name):
@@ -30,36 +18,48 @@ def finder(name):
     "XVid","sC0rp","PTpower","OSCARS","DXVA","MXMG","3LT0N","TiTAN","4PlayHD","HQ","HDRiP","MoH","MP4","BadMeetsEvil",
     "XViD","3Li","PTpOWeR","3D","HSBS","CC","RiPS","WEBRip","R5","PSiG","'GokU61","GB","GokU61","NL","EE","Rel","NL",
     "PSEUDO","DVD","Rip","NeRoZ","EXTENDED","DVDScr","xvid","WarrLord","SCREAM","MERRY","XMAS","iMB","7o9",
-    "Exclusive","171","DiDee","v2"
+    "Exclusive","171","DiDee","v2","mkv","brrip","HDTV","MB","HDRP","x265","EVO","ShAaNiG","BOKUTOX","XviD-ETRG","XviD",
+    "mp3","RARBG","PRiSTiNE","P2PDL","300mbfilms.com","dd72_","72_300","mbfilms.com","dd","mbfilms","WEB"
 
     ]
+
     year=0
-    for y in range(1900,2014):
+    for y in range(1000,2016):
         if str(y) in name:
             name = name.replace(str(y)," ")
             year = y
             break
     for value in replace:
-        name = name.replace(value," ")
-
+        name = name.replace(value," ")	
     name=name.lstrip()
-    name=name.rstrip()
-
+    name=name.rstrip()	
+    
     if year!=0:
-        url = "http://www.omdbapi.com/?t="+name+"&y="+str(year)
-        response = urllib.urlopen(url).read()
-        jsonvalues = json.loads(response)
-        if jsonvalues["Response"]=="True":
-            movieurl = "www.imdb.com/title/" + jsonvalues['imdbID']
-            webbrowser.open_new_tab(movieurl)
-
+        url = "http://www.omdbapi.com/?t="+name+"&y="+str(year)+"&type=movie"
+        try:
+            abc = urllib.request.urlopen(url).read()
+            w = json.loads(abc.decode('utf-8'))
+            if w["Response"]=="True":
+                movieurl = "www.imdb.com/title/" + w['imdbID']
+                webbrowser.open_new_tab(movieurl)
+        except urllib.error.HTTPError as e:
+            if e.code==400:
+               movieurl =  "http://www.imdb.com/find?q="+name+"&s=tt&ttype=ft&ref_=fn_ft"
+               webbrowser.open_new_tab(movieurl)
+            
     else:
-        url = "http://www.omdbapi.com/?t="+name
-        response = urllib.urlopen(url).read()
-        jsonvalues = json.loads(response)
-        if jsonvalues["Response"]=="True":
-            movieurl = "www.imdb.com/title/" + jsonvalues['imdbID']
-            webbrowser.open_new_tab(movieurl)
-
+        url = "http://www.omdbapi.com/?t="+name+"&type=movie"
+        try:
+            abc = urllib.request.urlopen(url).read()
+            w = json.loads(abc.decode('utf-8'))
+            if w["Response"]=="True":
+                movieurl = "www.imdb.com/title/" + w['imdbID']
+                webbrowser.open_new_tab(movieurl)
+        except urllib.error.HTTPError as e:
+            if e.code==400:
+               movieurl =  "http://www.imdb.com/find?q="+name+"&s=tt&ttype=ft&ref_=fn_ft"
+               webbrowser.open_new_tab(movieurl)
+            
+    
 file = sys.argv[1].split("\\")[-1]
 finder(file)
